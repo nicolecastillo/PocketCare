864211bf07ea92a9ad4f9b10e1114b146e4ac62d
RC2 <cryszarce2@gmail.com>
2019-04-26 22:41:38 -0700

    Grupos
    Agregue la opcion de qu

--- grupos.php
+++ grupos.php
@@ -8,6 +8,8 @@
 	<title>Grupos</title>
 	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 	<link rel="stylesheet" href="assets/css/style.css">
+	<script src="./javascripts/application.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 </head>
 <body>
 	<?php require 'partials/header.php'?>
@@ -15,6 +17,42 @@
 	<a href="index.php" ><img src="images/logo_1.png" alt="" /></a>
 
 	<h1>Grupos</h1>
+
+	<div style="text-align:center;">
+		<table border="1" style="margin: 0 auto;">
+			<caption>Grupos Activos</caption>
+			<thead>
+				<tr>
+					<th width="100">ID</th>
+					<th width="250">Grupo</th>
+					<th width="100">Alumnos</th>
+					<th width="100">Eventos</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php 
+					$sql = "SELECT * FROM grupos";
+					$stmt = $conn->query($sql);
+					
+					while ($_POST = $stmt->fetch(PDO::FETCH_ASSOC)) {
+				?>
+						<tr >
+							<td><?php echo $_POST["ID"];?></td>
+							<td><?php echo $_POST["NOMBRE"];?></td>
+							<td><?php echo $_POST["ALUMNOS"];?></td>
+							<td><?php echo $_POST["EVENTOS"];?></td>
+							<td>
+								<a href="#" onclick="preguntar(<?php echo  $_POST["ID"]; ?>)">Eliminar</a>
+							</td>
+						</tr>
+						<?php	
+					}
+					?>
+				
+			</tbody>
+		</table>
+	</div>
+
 	<form action="grupos.php" method="POST">
 		<nav>
 				<a href="registroGrupo.php">Registrar grupo</a></li>
@@ -22,5 +60,14 @@
 		</nav> 
 	</form>
 	
+	<script type="text/javascript">
+		function preguntar(id){
+			if(confirm("¿Eliminar " + id + "?")){
+				window.location.href = "borrarGrupo.php?id=" + id;
+			}
+		}
+		
+	</script>
+
 </body>
 </html>
