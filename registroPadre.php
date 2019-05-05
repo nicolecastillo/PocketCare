0b4e25a744bd236d66e6b0084d4d3989aad136b1
GerardoLeyvaConde <33427339+GerardoLeyvaConde@users.noreply.github.com>
2019-05-05 16:26:06 -0500

    Agregar email al registrar padre

--- registroPadre.php
+++ registroPadre.php
@@ -3,14 +3,14 @@
 	$message = '';
 	$dato = '';
 
-	if(!empty($_POST['Nombre']) && !empty($_POST['Apellido']) && !empty($_POST['Apellido2']) && !empty($_POST['usuario']) && !empty($_POST['contraseña']) && !empty($_POST['idHijo'])){
+	if(!empty($_POST['Nombre']) && !empty($_POST['Apellido']) && !empty($_POST['Apellido2']) && !empty($_POST['usuario']) && !empty($_POST['contraseña']) && !empty($_POST['idHijo']) && !empty($_POST['email'])){
 
 		$sql = "SELECT * FROM alumnos WHERE id = '$_POST[idHijo]'";
 		$stmt = $conn->prepare($sql);
 		$stmt->execute();
 		$resul = $stmt->fetchAll();
 		if($resul != NULL){
-			$sql = "INSERT INTO cuentas (tipocuenta, nombre, nombre2, apellido, apellido2, nombreusuario, contrasena, hijos) VALUES('0', '$_POST[Nombre]', '$_POST[Nombre2]', '$_POST[Apellido]', '$_POST[Apellido2]', '$_POST[usuario]', '$_POST[contraseña]', '$_POST[idHijo]')";
+			$sql = "INSERT INTO cuentas (tipocuenta, nombre, nombre2, apellido, apellido2, nombreusuario, contrasena, hijos, correo) VALUES('0', '$_POST[Nombre]', '$_POST[Nombre2]', '$_POST[Apellido]', '$_POST[Apellido2]', '$_POST[usuario]', '$_POST[contraseña]', '$_POST[idHijo]', '$_POST[email]')";
 
 			$stmt = $conn->prepare($sql);
 			if($stmt->execute()){
@@ -54,6 +54,7 @@
 		<input name="Nombre2" type="text" placeholder="Segundo nombre (opcional)">
 		<input name="Apellido" type="text" placeholder="Primer apellido">
 		<input name="Apellido2" type="text" placeholder="Segundo apellido">
+		<input name="email" type="text" placeholder="Correo electronico">
 		<input name="usuario" type="text" placeholder="Nombre de usuario">
 		<input name="contraseña" type="password" placeholder="Contraseña">
 		<input name="ConfirmContraseña" type="password" placeholder="Contraseña">
