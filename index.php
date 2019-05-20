857de30c858bf2d76b52522b44529d2bdfe2e2b1
Nicole Castillo <33427324+nicolecastillo@users.noreply.github.com>
2019-05-20 10:40:17 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+<?php  
 	session_start();
 
 	require 'basedatos.php';
@@ -23,12 +23,9 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 	<title>Bienvenido a PocketCare</title>
-	<link rel="stylesheet" href="assets/css/main.css" />
+	<link rel="stylesheet" href="assets/css/main.css" >
 
-	<!-- CALENDARIO PADRES -->
-	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Calendario</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 	<script src="js/jquery.min.js"></script>
 	<script src="js/moment.min.js"></script>
@@ -41,15 +38,13 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 	
 	<script src="js/bootstrap-clockpicker.js"></script>
-	<link rel="stylesheet" href="css/bootstrap-clockpicker.css">
-
+	<link rel="stylesheet" href="css/bootstrap-clockpicker.css">/>
 </head>
 <body>
-
+	
 	<?php if(!empty($user)): ?>
 		<?php if($results['tipocuenta'] == 1): ?>
 			<body class="homepage is-preload">
-			<div id="page-wrapper">
 
 				<!-- Header -->
 					<section id="header">
@@ -76,163 +71,9 @@
 									<p>Has ingresado satisfactoriamente</p>
 								</header>
 							</section>
-	
-<script>
-	$(document).ready(function(){
-		$('#Calendario').fullCalendar({
-			header:{
-				left:'today,prev,next',
-				center:'title',
-				right:'month,basicWeek,basicDay,agendaWeek,agendaDay'
-			},
-			
-			dayClick:function(date,jsEvent,view){
-				$('#btnAgregar').prop("disabled", false);
-				$('#btnModificar').prop("disabled", true);
-				$('#btnEliminar').prop("disabled", true);
-				limpiarFormulario();
-				$('#txtFecha').val(date.format());
-				$("#ModalEventos").modal();
-			},
-			
-			events: 'http://localhost/PocketCare/eventos.php',
-			eventClick:function(calEvent,jsEvent,view){
-				$('#btnAgregar').prop("disabled", true);
-				$('#btnModificar').prop("disabled", false);
-				$('#btnEliminar').prop("disabled", false);
-				// H2
-				$('#tituloEvento').html(calEvent.title);
-				// Mostrar la información del evento en los inputs
-				$('#txtDescripcion').val(calEvent.descripcion);
-				$('#txtID').val(calEvent.id);
-				$('#txtTitulo').val(calEvent.title);
-				$('#txtColor').val(calEvent.color);
-				FechaHora = calEvent.start._i.split(" ");
-				$('#txtFecha').val(FechaHora[0]);
-				$("#ModalEventos").modal();
-			},
-			editable:true,
-			eventDrop:function(calEvent){
-				$('#txtID').val(calEvent.id);
-				$('#txtTitulo').val(calEvent.title);
-				$('#txtColor').val(calEvent.color);
-				$('#txtDescripcion').val(calEvent.descripcion);
-				var fechaHora = calEvent.start.format().split("T");
-				$('#txtFecha').val(fechaHora[0]);
-				$('#txtHora').val(fechaHora[1]);
-				RecolectarDatosGUI();
-				EnviarInformacion('modificar', NuevoEvento, true);
-			}
-		});
-	});
-	
-</script>
+					</section>
 
 
-<!-- Modal(Agregar, Modificar, Eliminar) -->
-<div class="modal fade" id="ModalEventos" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="tituloEvento"></h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-
-      	<input type="hidden" id="txtID" name="txtID"/>
-      	<input type="hidden" id="txtFecha" name="txtFecha"/>
-		<div class="form-row">
-			<div class="form-group col-md-8">
-				<label>Título:</label>
-				<input type="text" id="txtTitulo" class="form-control" placeholder="Título del evento"/>
-			</div>
-			<div class="form-group col-md-4">
-				<label>Hora del evento: </label>
-				<div class="input-group clockpicker" data-autoclose="true">
-					<input type="text" id="txtHora" value="10:30" class="form-control" />
-				</div>
-			</div>
-		</div>
-		<div class="form-group">
-			<label>Descripción:</label>
-			<textarea id="txtDescripcion" rows="3" class="form-control"></textarea>
-		</div>
-		<div class="form-group">
-			<label>Color:</label>
-			<input type="color" value="#ff0000" id="txtColor" class="form-control" style="height: 36px;" />
-		</div>
-
-      </div>
-      <div class="modal-footer">
-      	<button type="button" id="btnAgregar" class="btn btn-success">Agregar</button>
-      	<button type="button" id="btnModificar" class="btn btn-success">Modificar</button>
-      	<button type="button" id="btnEliminar" class="btn btn-danger">Borrar</button>
-        <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-<script>
-	
-function ventanaNueva(){	
-	window.open('EnviarCorreo.php');
-}
-var NuevoEvento;
-$('#btnAgregar').click(function(){
-	RecolectarDatosGUI();
-	ventanaNueva();
-	EnviarInformacion('agregar', NuevoEvento);	
-});
-$('#btnEliminar').click(function(){
-	RecolectarDatosGUI();
-	ventanaNueva();
-	EnviarInformacion('eliminar', NuevoEvento);	
-});
-$('#btnModificar').click(function(){
-	RecolectarDatosGUI();
-	ventanaNueva();
-	EnviarInformacion('modificar', NuevoEvento);	
-});
-function RecolectarDatosGUI(){
-	NuevoEvento = {
-		id: $('#txtID').val(),
-		title: $('#txtTitulo').val(),
-		start: $('#txtFecha').val() + " " + $('#txtHora').val(),
-		color: $('#txtColor').val(),
-		descripcion: $('#txtDescripcion').val(),
-		textColor: "#FFFFFF",
-		end: $('#txtFecha').val() + " " + $('#txtHora').val()
-	};
-}
-function EnviarInformacion(accion, objEvento, modal){
-	$.ajax({
-		type: 'POST',
-		url: 'eventos.php?accion=' + accion,
-		data: objEvento,
-		success:function(msg){
-			if(msg){
-				$('#Calendario').fullCalendar('refetchEvents');
-				if(!modal){
-					$("#ModalEventos").modal('toggle');
-				}
-			}
-		},
-		error:function(){
-			alert("Hay un error ..");
-		}
-	});
-}
-$('.clockpicker').clockpicker();
-function limpiarFormulario(){
-	$('#txtID').val('');
-	$('#txtTitulo').val('');
-	$('#txtColor').val('');
-	$('#txtDescripcion').val('');
-}
-</script>	
 		</body>
 		<?php else: ?>
 			<body class="homepage is-preload">
@@ -287,7 +128,6 @@ function limpiarFormulario(){
 											},
 											
 											events: 'http://localhost/PocketCare/eventos.php',
-
 											eventClick:function(calEvent,jsEvent,view){
 												// H2
 												$('#tituloEvento').html(calEvent.title);
@@ -339,9 +179,7 @@ function limpiarFormulario(){
 								</div>
 
 								<script>
-
 								var NuevoEvento;
-
 								function RecolectarDatosGUI(){
 									NuevoEvento = {
 										id: $('#txtID').val(),
@@ -353,11 +191,10 @@ function limpiarFormulario(){
 										end: $('#txtFecha').val() + " " + $('#txtHora').val()
 									};
 								}
-
 								</script>
 							</body>
 		</body> 
-	<?php endif; ?>
+		<?php endif; ?>
 	<?php else: ?>
 
 		<body class="homepage is-preload">
@@ -378,7 +215,7 @@ function limpiarFormulario(){
 							</nav>
 
 						<!-- Banner -->
-
+							
 
 					</section>
 
@@ -394,6 +231,6 @@ function limpiarFormulario(){
 
 		</body>
 	<?php endif; ?>
-
+	
 </body>
 </html>
